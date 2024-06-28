6d06234fea15f11ca7df29e0fa5459999d74bda8
Mohsinmehmood-dev <mohsinmehmood.dev@gmail.com>
2024-06-28 22:00:57 +0500

    jwt and passport auth

--- src/root.module.ts
+++ src/root.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app/app.controller';
 @Module({
-  imports: [BooksModule],
-  controllers: [],
+  imports: [BooksModule, AuthModule, UsersModule],
+  controllers: [AppController],
   providers: [],
   exports: [],
 })
